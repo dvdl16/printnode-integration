f289fa80257990db6d739983c10b566c4d7d1683
Dirk van der Laarse <dirk@laarse.co.za>
2020-05-27 12:44:44 +0200

    fix: new way to parse and install requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@
 from setuptools import setup, find_packages
 import re, ast
 
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
-
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in printnode_integration/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -14,8 +11,6 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('printnode_integration/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='printnode_integration',
