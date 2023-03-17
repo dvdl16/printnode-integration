0818a3279d4f0c1f9079edab4e4e0569dfcb4f6d
Dirk van der Laarse <dirk@laarse.co.za>
2023-03-17 21:32:31 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -21,5 +21,14 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[
+		'PrintNode-Python @ git+https://github.com/PrintNode/PrintNode-Python.git#egg=printnodeapi',
+		'frappe',
+		'pyqrcode',
+		'python-barcode',
+		'Pillow',
+		'zpl',
+		'numpy',
+		'scipy'
+	]
 )
