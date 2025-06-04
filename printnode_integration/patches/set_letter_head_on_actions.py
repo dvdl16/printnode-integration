070688fc5d66cf39d544f2dc3ff1ff798c45c6c5
Dirk van der Laarse <dirk@laarse.co.za>
2025-06-04 12:49:35 +0000

    feat: configurable letter head per action

---
+++ printnode_integration/patches/set_letter_head_on_actions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import frappe
+
+
+def execute():
+	"""
+	For all Print Node Actions, if 'is_raw_text' is 0, set 'apply_letter_head' to 1
+	"""
+	frappe.db.sql("UPDATE `tabPrint Node Action` SET apply_letter_head = 1 WHERE is_raw_text = 0")
+	frappe.db.commit()
