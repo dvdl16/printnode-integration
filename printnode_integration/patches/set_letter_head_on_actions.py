934b028614f79c512a88962b4d40434aefd1f386
Dirk van der Laarse <dirk@laarse.co.za>
2025-06-04 13:39:20 +0000

    fix: add doctype reload to patch

--- printnode_integration/patches/set_letter_head_on_actions.py
+++ printnode_integration/patches/set_letter_head_on_actions.py
@@ -9,5 +9,7 @@ def execute():
 	"""
 	For all Print Node Actions, if 'is_raw_text' is 0, set 'apply_letter_head' to 1
 	"""
+	frappe.reload_doctype("Print Node Action")
+
 	frappe.db.sql("UPDATE `tabPrint Node Action` SET apply_letter_head = 1 WHERE is_raw_text = 0")
 	frappe.db.commit()
